e83618e8b2ca4824f46320bd44216281d898ef3f
David Zhang <cajan2@163.com>
2017-05-03 22:07:49 +0800

    ch11 completed

--- educa/educa/config_cache.py
+++ educa/educa/config_cache.py
@@ -4,3 +4,7 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
+CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
